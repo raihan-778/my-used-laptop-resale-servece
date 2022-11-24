ae221e5677ae982a2b5506854582e2ba32780fe0
unknown <raihanarif778@gmail.com>
2022-11-24 08:20:46 +0600

    react-roter-dom daisyUi & firebase installed

---
+++ src/Dashboard/AllSeller/AllSeller.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AllSeller = () => {
+  return (
+    <div>
+      <h2>AllSeller info</h2>
+    </div>
+  );
+};
+
+export default AllSeller;
---
+++ src/Dashboard/Allusers/Allusers.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Allusers = () => {
+  return (
+    <div>
+      <h2>All users Info</h2>
+    </div>
+  );
+};
+
+export default Allusers;
