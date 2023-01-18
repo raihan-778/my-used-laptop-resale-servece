e5d18857c8c289e886f085b8a7d954c7bb64f751
unknown <raihanarif778@gmail.com>
2023-01-18 21:42:41 +0600

    design changes

--- src/pages/Home/Header/Header.js
+++ src/pages/Home/Header/Header.js
@@ -6,35 +6,34 @@ import {
   Zoom,
 } from "react-awesome-reveal";
 import { Link } from "react-router-dom";
+import Banner from "../Banner/Banner";
 
 const Header = () => {
   return (
     <div
-      className="hero min-h-screen mb-5 rounded-xl"
+      className="hero min-h-screen mb-5 rounded-xl "
       style={{ backgroundImage: `url("https://i.ibb.co/sK751gv/header2.jpg")` }}
     >
-      <div className="hero-overlay rounded-xl bg-opacity-60"></div>
-      <div className="hero-content text-center text-neutral-content">
-        <div className="max-w-md">
-          <JackInTheBox>
-            <h1 className="mb-5 text-5xl text-slate-100 font-bold">
-              Welcome to your trusted Laptop Re-sele Shop
-            </h1>
-          </JackInTheBox>
-          <Fade delay={1e3} cascade damping={1e-1}>
-            <p className="mb-5 font-semibold text-indigo-200">
-              We sell best quality used laptop with resonable price.We believe
-              in quality and customers trust.Your faith is Our assets.
-            </p>
-          </Fade>
+      <div className="w-3/4 max-w-md">
+        <JackInTheBox>
+          <h1 className="mb-5 text-5xl text-slate-100 font-bold">
+            Welcome to your trusted Laptop Re-sele Shop
+          </h1>
+        </JackInTheBox>
+        <Fade delay={1e3} cascade damping={1e-1}>
+          <p className="mb-5 font-semibold text-indigo-200">
+            We sell best quality used laptop with resonable price.We believe in
+            quality and customers trust.Your faith is Our assets.
+          </p>
+        </Fade>
 
-          <Link to="/categories">
-            <Zoom>
-              <button className="btn  btn-primary">Visit Shop</button>
-            </Zoom>
-          </Link>
-        </div>
+        <Link to="/categories">
+          <Zoom>
+            <button className="btn  btn-primary">Visit Shop</button>
+          </Zoom>
+        </Link>
       </div>
+      <div className="hero-overlay rounded-xl bg-opacity-60"></div>
     </div>
   );
 };
